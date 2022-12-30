8b4ab8b8d4600c5a962b3f5af58dc0c9f02fc8dc
kames <kamescg@gmail.com>
2022-12-30 17:50:37 -0300

    fix: types

--- pages/[chainId]/erc721/[address]/[tokenId].tsx
+++ pages/[chainId]/erc721/[address]/[tokenId].tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { ERC721Attributes, ERC721Description, ERC721Image, ERC721Name } from '@turbo-eth/erc721-wagmi'
 import { useRouter } from 'next/router'
 
@@ -5,9 +6,7 @@ import { Head } from 'components/layout/Head'
 
 export default function Home() {
   const router = useRouter()
-
   const { address, chainId, tokenId } = router.query
-  console.log(tokenId, 'tokenIdtokenId')
   return (
     <>
       <Head />
@@ -15,17 +14,22 @@ export default function Home() {
         <div className="flex-center flex h-full flex-1 flex-col items-center justify-center ">
           <div className="card w-[420px] ">
             <h3 className="mb-3 text-2xl font-normal">
-              <ERC721Name chainId={chainId} tokenId={tokenId || '2'} address={String(address || '')} />
+              <ERC721Name chainId={chainId} tokenId={String(tokenId || '1')} address={String(address || '')} />
             </h3>
-            <ERC721Image address={String(address || '2')} tokenId={tokenId || '2'} className="my-4 rounded-xl border-2 border-white shadow-md" />
+            <ERC721Image
+              address={String(address || '1')}
+              tokenId={String(tokenId || '1')}
+              className="my-4 rounded-xl border-2 border-white shadow-md"
+            />
             <p className="text-xs leading-5">
-              <ERC721Description tokenId={tokenId || '2'} address={String(address || '')} />
+              <ERC721Description chainId={chainId} tokenId={String(tokenId || '1')} address={String(address || '')} />
             </p>
             <hr className="my-4" />
             <ERC721Attributes
+              chainId={chainId}
               classNameValue="py-1 flex justify-between py-3 font-bold"
               classNameLabel="font-light"
-              tokenId={tokenId || '2'}
+              tokenId={String(tokenId || '1')}
               address={String(address || '')}
             />
           </div>
