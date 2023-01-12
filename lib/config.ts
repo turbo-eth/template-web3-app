aa1e221c6a37f52fe734f80e64a25a49251d7bd9
kames <kamescg@gmail.com>
2023-01-12 16:07:43 -0300

    fix: wrong config @2

--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,21 @@
 import { IronSessionOptions } from 'iron-session'
+import { SiweMessage } from 'siwe'
+
+declare module 'iron-session' {
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  interface IronSessionData {
+    nonce: string
+    siwe: SiweMessage
+    isAdmin: boolean
+  }
+}
 
 export const SITE_NAME = 'TurboETH'
 
 export const SERVER_SESSION_SETTINGS: IronSessionOptions = {
   cookieName: SITE_NAME,
-  password: process.env.SESSION_PASSWORD ?? 'UPDATE_TO_complex_password_at_least_32_characters_long',
+  password: process.env.NEXT_AUTH_SECRET ?? 'UPDATE_TO_complex_password_at_least_32_characters_long',
   cookieOptions: {
-    secure: false,
+    secure: process.env.NODE_ENV == 'production',
   },
 }
