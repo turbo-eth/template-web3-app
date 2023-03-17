0b3d6978ac6581a8d218a29c5da0b486c057505f
JustBassey <justinbassey@gmail.com>
2023-03-17 17:00:20 -0400

    feat: add base testnet

--- config/networks.ts
+++ config/networks.ts
@@ -1,7 +1,7 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // Networks
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-import { arbitrum, arbitrumGoerli, goerli, hardhat, mainnet, optimism, optimismGoerli, polygon, sepolia } from '@wagmi/chains'
+import { arbitrum, arbitrumGoerli, baseGoerli, goerli, hardhat, mainnet, optimism, optimismGoerli, polygon, sepolia } from '@wagmi/chains'
 import { configureChains } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { infuraProvider } from 'wagmi/providers/infura'
@@ -11,8 +11,12 @@ import { publicProvider } from 'wagmi/providers/public'
 goerli.iconUrl = '/icons/NetworkEthereumTest.svg'
 // @ts-ignore
 sepolia.iconUrl = '/icons/NetworkEthereumTest.svg'
+// @ts-ignore
+arbitrumGoerli.iconUrl = '/icons/NetworkArbitrumTest.svg'
+// @ts-ignore
+baseGoerli.iconUrl = '/icons/NetworkBaseTest.svg'
 
-export const ETH_CHAINS_TEST = [mainnet, goerli, sepolia, optimismGoerli, arbitrumGoerli, hardhat]
+export const ETH_CHAINS_TEST = [mainnet, goerli, sepolia, optimismGoerli, arbitrumGoerli, baseGoerli, hardhat]
 export const ETH_CHAINS_PROD = [mainnet, optimism, arbitrum, polygon]
 
 export const CHAINS = process.env.NODE_ENV === 'production' ? ETH_CHAINS_PROD : ETH_CHAINS_TEST
@@ -35,9 +39,5 @@ if (process.env.NEXT_PUBLIC_INFURA_API_KEY) {
   )
 }
 
-if (process.env.NEXT_PUBLIC_PROVIDER_PUBLIC) {
-  PROVIDERS.push(publicProvider())
-}
-
 // @ts-ignore
-export const { chains, provider } = configureChains(CHAINS, PROVIDERS)
+export const { chains, provider } = configureChains(CHAINS, [...PROVIDERS, publicProvider()])
