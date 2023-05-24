4a44307a30bc2d19a4c499be2436aef93931ed49
MAlvarez <31598428+MAlvarez-0@users.noreply.github.com>
2023-05-24 13:00:01 +0200

    Update app/(general)/integration/pooltogether-v4/layout.tsx
    Co-authored-by: Vitor Marthendal Nunes <marthendalnunes@gmail.com>

--- app/(general)/integration/pooltogether-v4/layout.tsx
+++ app/(general)/integration/pooltogether-v4/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { ReactNode } from 'react'
 
-import classNames from 'clsx'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
