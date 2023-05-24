2d5946b6f413b37376f6f00ae8e95e2cddc28fbd
MAlvarez <31598428+MAlvarez-0@users.noreply.github.com>
2023-05-24 13:00:25 +0200

    Update integrations/pooltogether-v4/components/form-yield-source-prize-pool-deposit.tsx
    Co-authored-by: Vitor Marthendal Nunes <marthendalnunes@gmail.com>

--- integrations/pooltogether-v4/components/form-yield-source-prize-pool-deposit.tsx
+++ integrations/pooltogether-v4/components/form-yield-source-prize-pool-deposit.tsx
@@ -100,7 +100,7 @@ export function PoolTogetherFormDeposit() {
         <Form.Field name="amountDeposit">
           <div className="flex justify-between align-baseline">
             <Form.Label className="mb-2">Amount</Form.Label>
-            <Form.Label className="FormLabel mb-2">
+            <Form.Label className="mb-2">
               <a className="ml-10 cursor-pointer hover:underline" onClick={() => handleAmount()}>
                 {parseFloat(userBalance.toString()).toFixed(2)} USDC
               </a>
