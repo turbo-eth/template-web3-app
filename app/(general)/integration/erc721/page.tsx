a96ab9786f73efbfd96cae15df9406065372df54
Vitor <marthendalnunes@gmail.com>
2023-06-02 17:27:08 -0300

    feat: add erc721 page

---
+++ app/(general)/integration/erc721/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+import { motion } from 'framer-motion'
+import Balancer from 'react-wrap-balancer'
+
+import { WalletConnect } from '@/components/blockchain/wallet-connect'
+import { BranchIsWalletConnected } from '@/components/shared/branch-is-wallet-connected'
+import { LinkComponent } from '@/components/shared/link-component'
+import { FADE_DOWN_ANIMATION_VARIANTS } from '@/config/design'
+import { turboIntegrations } from '@/data/turbo-integrations'
+import { ERC721Deploy, Erc721Read, Erc721WriteApprove, Erc721WriteMint, Erc721WriteTransfer } from '@/integrations/erc721'
+import { useErc721TokenStorage } from '@/integrations/erc721/hooks/use-erc721-token-storage'
+
+export default function PageIntegration() {
+  const [token] = useErc721TokenStorage()
+
+  return (
+    <>
+      <div className="flex-center flex flex-1 flex-col items-center justify-center">
+        <motion.div
+          className="max-w-screen-xl px-5 text-center xl:px-0"
+          initial="hidden"
+          whileInView="show"
+          animate="show"
+          viewport={{ once: true }}
+          variants={{
+            hidden: {},
+            show: {
+              transition: {
+                staggerChildren: 0.15,
+              },
+            },
+          }}>
+          <motion.h1
+            className="text-gradient-sand my-4 text-center text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-8xl md:leading-[6rem]"
+            variants={FADE_DOWN_ANIMATION_VARIANTS}>
+            {turboIntegrations.erc721.name}
+          </motion.h1>
+          <motion.p className="my-4 text-lg" variants={FADE_DOWN_ANIMATION_VARIANTS}>
+            <Balancer>{turboIntegrations.erc721.description}</Balancer>
+          </motion.p>
+          <motion.div className="my-4 text-xl" variants={FADE_DOWN_ANIMATION_VARIANTS}>
+            <LinkComponent isExternal href={turboIntegrations.erc721.url}>
+              <button className="btn btn-primary">Documentation</button>
+            </LinkComponent>
+          </motion.div>
+        </motion.div>
+      </div>
+      <section className="w-full lg:mt-10">
+        <div className="container flex w-full flex-col items-center">
+          <BranchIsWalletConnected>
+            <div className="flex w-full max-w-screen-lg flex-col gap-y-8">
+              <ERC721Deploy />
+              {token && (
+                <>
+                  <Erc721Read />
+                  <Erc721WriteMint />
+                  <Erc721WriteApprove />
+                  <Erc721WriteTransfer />
+                </>
+              )}
+            </div>
+            <WalletConnect />
+          </BranchIsWalletConnected>
+        </div>
+      </section>
+    </>
+  )
+}
