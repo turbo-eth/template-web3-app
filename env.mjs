62fdfdbd3287ec69088c8dd1565e16b5f1b12028
Vitor <marthendalnunes@gmail.com>
2023-06-06 09:36:02 -0300

    fix: make NEXT_PUBLIC_USE_PUBLIC_PROVIDER default true and add regex to APP_ADMINS

--- env.mjs
+++ env.mjs
@@ -6,11 +6,15 @@ export const env = createEnv({
     // Iron session requires a secret of at least 32 characters
     NEXTAUTH_SECRET: z.string().min(32),
     DATABASE_URL: z.string().url().optional(),
-    APP_ADMINS: z.string().min(1).optional(),
+    // Comma separated list of Ethereum addresses, accepts optinal whitespace after comma
+    APP_ADMINS: z
+      .string()
+      .regex(/^(0x[a-fA-F0-9]{40}( *, *0x[a-fA-F0-9]{40})* *)*$/)
+      .optional(),
     SITE_URL: z.string().url().optional(),
   },
   client: {
-    NEXT_PUBLIC_USE_PUBLIC_PROVIDER: z.boolean().default(false),
+    NEXT_PUBLIC_USE_PUBLIC_PROVIDER: z.boolean().default(true),
     NEXT_PUBLIC_USE_HARDHAT_PROVIDER: z.boolean().default(false),
     NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1).optional(),
     NEXT_PUBLIC_INFURA_API_KEY: z.string().min(1).optional(),
