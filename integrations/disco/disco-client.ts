5c7bc1cb1804e999115122d5707ba48cb2a1a485
Vitor <marthendalnunes@gmail.com>
2023-06-06 21:51:33 -0300

    fix: move disco and etherscan env variables to public

--- integrations/disco/disco-client.ts
+++ integrations/disco/disco-client.ts
@@ -6,6 +6,6 @@ export const discoClient = axios.create({
   baseURL: 'https://api.disco.xyz/v1',
   timeout: 20000,
   headers: {
-    Authorization: `Bearer ${env.DISCO_API_KEY}`,
+    Authorization: `Bearer ${env.NEXT_PUBLIC_DISCO_API_KEY}`,
   },
 })
