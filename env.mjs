34dd8db01d510d902ebbf70c43d252fa7b6e7cd4
Vitor <marthendalnunes@gmail.com>
2023-06-15 19:06:39 -0300

    chore: add default value to NEXTAUTH_SECRET

--- env.mjs
+++ env.mjs
@@ -4,7 +4,7 @@ import { z } from 'zod'
 export const env = createEnv({
   server: {
     // Iron session requires a secret of at least 32 characters
-    NEXTAUTH_SECRET: z.string().min(32),
+    NEXTAUTH_SECRET: z.string().min(32).default('complex_password_at_least_32_characters_long'),
     DATABASE_URL: z.string().url().optional(),
     // Comma separated list of Ethereum addresses, accepts optinal whitespace after comma
     APP_ADMINS: z
