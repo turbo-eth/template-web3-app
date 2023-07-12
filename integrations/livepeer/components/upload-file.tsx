f5db8d107d904444b9bca1712af7b650fd8778dd
luiza <63298341+luizakp@users.noreply.github.com>
2023-07-12 10:21:36 -0300

    fix: remove useEffect from upload-file
    Co-authored-by: Vitor Marthendal Nunes <marthendalnunes@gmail.com>

--- integrations/livepeer/components/upload-file.tsx
+++ integrations/livepeer/components/upload-file.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 
 import { useCreateAsset } from '@livepeer/react'
 import { useRouter } from 'next/navigation'
