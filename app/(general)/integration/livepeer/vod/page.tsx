227886018140bba183f29913030d83620e89067d
luiza <63298341+luizakp@users.noreply.github.com>
2023-07-12 10:19:03 -0300

    fix: remove const from inside the component
    Co-authored-by: Vitor Marthendal Nunes <marthendalnunes@gmail.com>

--- app/(general)/integration/livepeer/vod/page.tsx
+++ app/(general)/integration/livepeer/vod/page.tsx
@@ -6,10 +6,9 @@ import { LinkComponent } from '@/components/shared/link-component'
 import { FormLivepeerApiKey } from '@/integrations/livepeer/components/form-livepeer-api-key'
 import { useIsLivepeerApiKeySet } from '@/integrations/livepeer/hooks/use-livepeer-api-key'
 
+const newVodPath = '/integration/livepeer/vod/new'
+const watchVodPath = '/integration/livepeer/vod/watch'
 export default function PageIntegration() {
-  const newVodPath = '/integration/livepeer/vod/new'
-  const watchVodPath = '/integration/livepeer/vod/watch'
-
   const [isLoadingNewVideo, setIsLoadingNewVideo] = useState<boolean>(false)
   const [isLoadingExistingVideo, setIsLoadingExistingVideo] = useState<boolean>(false)
   const isLivepeerApiKeySet = useIsLivepeerApiKeySet()
