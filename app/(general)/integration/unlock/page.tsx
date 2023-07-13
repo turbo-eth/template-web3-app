469ef6e4ff31bbe5545bcfc0c5d3b70d02d78721
Thomas <tombutler4@hotmail.com>
2023-07-13 09:20:01 -0500

    feat: only show create lock form if wallet connected

--- app/(general)/integration/unlock/page.tsx
+++ app/(general)/integration/unlock/page.tsx
@@ -1,10 +1,20 @@
 import FormDeployLock from "@/integrations/unlock/components/form-deploy-lock"
 
+
+import { WalletConnect } from '@/components/blockchain/wallet-connect'
+import { IsWalletConnected } from '@/components/shared/is-wallet-connected'
+import { IsWalletDisconnected } from '@/components/shared/is-wallet-disconnected'
+
 export default function UnlockIntegration() {
   return (
     <div>
       <h1>Unlock Integration</h1>
-      <FormDeployLock />
+      <IsWalletConnected>
+        <FormDeployLock />
+      </IsWalletConnected>
+      <IsWalletDisconnected>
+        <WalletConnect />
+      </IsWalletDisconnected>
     </div>
   )
 }
