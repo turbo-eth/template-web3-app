f736a43d570f904c7537406abc209ca7e56885a2
Thomas <tombutler4@hotmail.com>
2023-07-13 18:47:48 -0500

    feat: able to deploy locks on goerli

--- integrations/unlock/components/form-deploy-lock.tsx
+++ integrations/unlock/components/form-deploy-lock.tsx
@@ -1,15 +1,27 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import { UnlockV12, PublicLockV13 } from '@unlock-protocol/contracts'
+import { ethers } from 'ethers'
+
+import {
+  useAccount, 
+  useContractWrite,
+  usePrepareContractWrite
+} from 'wagmi'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Switch } from '@/components/ui/switch'
 
-import UnlockProvider from '../unlock-provider'
 
-// NOTE: refer to -> https://docs.unlock-protocol.com/tutorials/front-end/paywall/provider
+const lockInterface = new ethers.utils.Interface(PublicLockV13.abi)
 
 export default function FormDeployLock() {
+  const { address: creator } = useAccount()
+
+  const [calldata, setCalldata] = useState<string>('')
   const [lockName, setLockName] = useState<string>('test lock')
   const [maxKeys, setMaxKeys] = useState<number>(10)
   const [lastMaxKeys, setLastMaxKeys] = useState<number>(0) // NOTE: used to store the last max keys before unlimited keys
@@ -19,44 +31,34 @@ export default function FormDeployLock() {
   const [unlimitedKeys, setUnlimitedKeys] = useState<boolean>(false)
   const [unlimitedDuration, setUnlimitedDuration] = useState<boolean>(false)
 
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const { provider } = UnlockProvider()
-
-  const handleCreateLock = async () => {
-    console.log(lockName, maxKeys, duration, keyPrice)
-    setIsLoading(true)
-    try {
-      console.log(provider)
-      /*
-      // connect to provider with wallet 
-      await walletService.connect(provider, wallet)
-
-       // This only resolves when the transaction has been mined, but the callback returns the hash immediately
-      await walletService.createLock(
-        {
-          maxNumberOfKeys: maxKeys,
-          name: lockName,
-          expirationDuration: duration,
-          keyPrice: keyPrice, // Key price needs to be a string
-        },
-        {}, // transaction options
-        (error: Error, hash: string) => {
-          // This is the hash of the transaction!
-          console.log({ hash })
-        }
+  useEffect(() => {
+    const prepareCalldata = async () => {
+      setCalldata(
+        lockInterface.encodeFunctionData('initialize(address,uint256,address,uint256,uint256,string)', [
+          creator,
+          unlimitedDuration ? ethers.constants.MaxUint256 : duration * 60 * 60 * 24, // duration in days
+          ethers.constants.AddressZero, // token address defaults to ETH
+          ethers.utils.parseUnits(keyPrice, 18), // key price in ETH
+          unlimitedKeys ? ethers.constants.MaxUint256 : maxKeys,
+          lockName,
+        ])
       )
-      */
-    } catch (error) {
-      console.log(error)
     }
-    setIsLoading(false)
-  }
+    prepareCalldata()
+  }, [creator, duration, keyPrice, maxKeys, lockName, unlimitedDuration, unlimitedKeys])
+
+  const { config } = usePrepareContractWrite({
+    address: '0x627118a4fB747016911e5cDA82e2E77C531e8206', // goerli unlock contract
+    abi: UnlockV12.abi,
+    functionName: 'createUpgradeableLockAtVersion',
+    args: [calldata, 12], // version 12
+  })
+  const { data, isLoading, isSuccess, write } = useContractWrite(config)
 
   const handleUnlimitedKeys = (e: boolean) => {
     setUnlimitedKeys(e)
     if (e == true) {
       setLastMaxKeys(maxKeys)
-      setMaxKeys(0) // TODO: update to what unlock expects for unlimited keys
     } else {
       setMaxKeys(lastMaxKeys)
     }
@@ -66,7 +68,6 @@ export default function FormDeployLock() {
     setUnlimitedDuration(e)
     if (e == true) {
       setLastDuration(duration)
-      setDuration(0) // TODO: update to what unlock expects for unlimited duration
     } else {
       setDuration(lastDuration)
     }
@@ -81,22 +82,32 @@ export default function FormDeployLock() {
       <div>
         <p>Max Number of Keys</p>
         <div className="flex flex-row items-center justify-center">
-          {unlimitedKeys ? <Input disabled={true} /> : <Input placeholder={maxKeys.toString()} onChange={(e) => setMaxKeys(Number(e.target.value))} />}
+          {unlimitedKeys ? (
+            <Input disabled={true} />
+          ) : (
+            <Input placeholder={maxKeys.toString()} onChange={(e) => setMaxKeys(Number(e.target.value))} />
+          )}
           unlimited <Switch onCheckedChange={(e) => handleUnlimitedKeys(e)} />
         </div>
       </div>
       <div>
         <p>Membership Duration (Days)</p>
         <div className="flex flex-row items-center justify-center">
-          {unlimitedDuration ? <Input disabled={true} /> : <Input placeholder={duration.toString()} onChange={(e) => setDuration(Number(e.target.value))} />}
+          {unlimitedDuration ? (
+            <Input disabled={true} />
+          ) : (
+            <Input placeholder={duration.toString()} onChange={(e) => setDuration(Number(e.target.value))} />
+          )}
           unlimited <Switch onCheckedChange={(e) => handleUnlimitedDuration(e)} />
         </div>
       </div>
       <div>
         <p>Key Price</p>
-        <Input placeHolder={keyPrice} onChange={(e) => setKeyPrice(Number(e.target.value))} />
+        <Input placeholder={keyPrice} onChange={(e) => setKeyPrice(e.target.value)} />
       </div>
-      <Button onClick={handleCreateLock}>{isLoading ? 'Creating Lock...' : 'Create Lock'}</Button>
+      <Button onClick={() => write?.()}>Deploy Lock</Button>
+      {isLoading && <p>Deploying Lock...</p>}
+      {isSuccess && <p>Lock Deployed!</p>}
     </div>
   )
 }
