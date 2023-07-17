0c83c99f394b5abf83c67c117a1d9fde35d105e0
luizakp <luiza.puccinelli06@gmail.com>
2023-07-17 19:47:39 -0300

    fix: remove loading from link component

--- app/(general)/integration/livepeer/livestream/page.tsx
+++ app/(general)/integration/livepeer/livestream/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { useState } from 'react'
-
 import { LinkComponent } from '@/components/shared/link-component'
 import { FormLivepeerApiKey } from '@/integrations/livepeer/components/form-livepeer-api-key'
 import { useIsLivepeerApiKeySet } from '@/integrations/livepeer/hooks/use-livepeer-api-key'
@@ -11,36 +9,24 @@ const newStreamBrowserPath = '/integration/livepeer/livestream/new/browser'
 const watchStreamPath = '/integration/livepeer/livestream/watch'
 
 export default function PageIntegration() {
-  const [isLoadingNewStreamObs, setIsLoadingNewStreamObs] = useState<boolean>(false)
-  const [isLoadingNewStreamBrowser, setIsLoadingNewStreamBrowser] = useState<boolean>(false)
-  const [isLoadingExistingStream, setIsLoadingExistingStream] = useState<boolean>(false)
   const isLivepeerApiKeySet = useIsLivepeerApiKeySet()
 
   return (
     <div className="card">
       {!isLivepeerApiKeySet && <FormLivepeerApiKey />}
       <LinkComponent href={newStreamObsPath}>
-        <button
-          className="btn btn-emerald mt-4 w-full"
-          disabled={!isLivepeerApiKeySet || isLoadingNewStreamObs}
-          onClick={() => setIsLoadingNewStreamObs(true)}>
-          {isLoadingNewStreamObs ? 'Loading...' : 'Create a new OBS livestream'}
+        <button className="btn btn-emerald mt-4 w-full" disabled={!isLivepeerApiKeySet}>
+          Create a new OBS livestream
         </button>
       </LinkComponent>
       <LinkComponent href={newStreamBrowserPath}>
-        <button
-          className="btn btn-emerald mt-4 w-full"
-          disabled={!isLivepeerApiKeySet || isLoadingNewStreamBrowser}
-          onClick={() => setIsLoadingNewStreamBrowser(true)}>
-          {isLoadingNewStreamBrowser ? 'Loading...' : 'Create a new Browser livestream'}
+        <button className="btn btn-emerald mt-4 w-full" disabled={!isLivepeerApiKeySet}>
+          Create a new Browser livestream
         </button>
       </LinkComponent>
       <LinkComponent href={watchStreamPath}>
-        <button
-          className="btn btn-emerald mt-4 w-full"
-          disabled={!isLivepeerApiKeySet || isLoadingExistingStream}
-          onClick={() => setIsLoadingExistingStream(true)}>
-          {isLoadingExistingStream ? 'Loading...' : 'Watch an existing livestream'}
+        <button className="btn btn-emerald mt-4 w-full" disabled={!isLivepeerApiKeySet}>
+          Watch an existing livestream
         </button>
       </LinkComponent>
     </div>
--- app/(general)/integration/livepeer/vod/page.tsx
+++ app/(general)/integration/livepeer/vod/page.tsx
@@ -1,34 +1,25 @@
 'use client'
 
-import { useState } from 'react'
-
 import { LinkComponent } from '@/components/shared/link-component'
 import { FormLivepeerApiKey } from '@/integrations/livepeer/components/form-livepeer-api-key'
 import { useIsLivepeerApiKeySet } from '@/integrations/livepeer/hooks/use-livepeer-api-key'
 
 const newVodPath = '/integration/livepeer/vod/new'
 const watchVodPath = '/integration/livepeer/vod/watch'
+
 export default function PageIntegration() {
-  const [isLoadingNewVideo, setIsLoadingNewVideo] = useState<boolean>(false)
-  const [isLoadingExistingVideo, setIsLoadingExistingVideo] = useState<boolean>(false)
   const isLivepeerApiKeySet = useIsLivepeerApiKeySet()
   return (
     <div className="card">
       {!isLivepeerApiKeySet && <FormLivepeerApiKey />}
       <LinkComponent href={newVodPath}>
-        <button
-          className="btn btn-emerald mt-4 w-full"
-          disabled={!isLivepeerApiKeySet || isLoadingNewVideo}
-          onClick={() => setIsLoadingNewVideo(true)}>
-          {isLoadingNewVideo ? 'Loading...' : 'Upload a new video'}
+        <button className="btn btn-emerald mt-4 w-full" disabled={!isLivepeerApiKeySet}>
+          Upload a new video
         </button>
       </LinkComponent>
       <LinkComponent href={watchVodPath}>
-        <button
-          className="btn btn-emerald mt-4 w-full"
-          disabled={!isLivepeerApiKeySet || isLoadingExistingVideo}
-          onClick={() => setIsLoadingExistingVideo(true)}>
-          {isLoadingExistingVideo ? 'Loading...' : 'Watch an existing video'}
+        <button className="btn btn-emerald mt-4 w-full" disabled={!isLivepeerApiKeySet}>
+          Watch an existing video
         </button>
       </LinkComponent>
     </div>
