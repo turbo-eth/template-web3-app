0efd528e332c4cda9ca7e6df79fdf3604f95bd04
AlanRacciatti <alanracciatti1220@gmail.com>
2023-07-17 18:50:07 -0300

    feat(ui): add supplied and borrowed assets lists

--- integrations/aave/components/general-info.tsx
+++ integrations/aave/components/general-info.tsx
@@ -1,12 +1,87 @@
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { FADE_DOWN_ANIMATION_VARIANTS } from '@/config/design'
+
 export const GeneralInfo = () => {
   return (
-    <div className="mb-4 flex items-center justify-between bg-gray-200 p-4 dark:bg-gray-700 dark:text-white">
-      <span>
-        Current Chain: <strong>Ethereum</strong>
-      </span>
-      <button className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-600">
-        Switch Chain
-      </button>
-    </div>
+    <motion.div
+      animate="show"
+      className="mb-4 flex items-start justify-start p-4 dark:text-white"
+      initial="hidden"
+      variants={FADE_DOWN_ANIMATION_VARIANTS}>
+      <div className="rounded border border-slate-200 p-5 dark:border-slate-600">
+        {/* Network Select */}
+        <div className="mb-4 flex items-center">
+          <div className="flex w-60 flex-col ">
+            <Select value="ethereum">
+              <SelectTrigger className="input mt-2 bg-white text-gray-600 placeholder:text-neutral-400 dark:bg-gray-700 dark:text-slate-300 dark:placeholder:text-neutral-400">
+                <SelectValue placeholder="Select market" />
+              </SelectTrigger>
+              <SelectContent className="w-56 bg-white dark:bg-gray-700">
+                <SelectItem value="ethereum">
+                  <div className="flex items-center justify-between">
+                    <Image
+                      alt="Ethereum"
+                      className="mr-2 rounded-full"
+                      height={30}
+                      src="/integrations/connext/logos/chains/ethereum.png"
+                      width={30}
+                    />
+                    Ethereum Market
+                  </div>
+                </SelectItem>
+                <SelectItem value="asdf">
+                  <div className="flex items-center justify-between">
+                    <Image
+                      alt="Ethereum"
+                      className="mr-2 rounded-full"
+                      height={30}
+                      src="/integrations/connext/logos/chains/ethereum.png"
+                      width={30}
+                    />
+                    Ethereum Market
+                  </div>
+                </SelectItem>
+                <SelectItem value="qwer">
+                  <div className="flex items-center justify-between">
+                    <Image
+                      alt="Ethereum"
+                      className="mr-2 rounded-full"
+                      height={30}
+                      src="/integrations/connext/logos/chains/ethereum.png"
+                      width={30}
+                    />
+                    Ethereum Market
+                  </div>
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        {/* Net Worth, Net APY, Health Factor */}
+        <div className="flex justify-between">
+          <div className="mr-3 text-slate-500 dark:text-slate-300">
+            <h3 className="mb-2">Net Worth</h3>
+            <p className="font-bold text-black dark:text-white">
+              <span className="text-slate-500 dark:text-slate-300">$</span>1.000
+            </p>{' '}
+            {/* replace with actual value */}
+          </div>
+          <div className="mr-3 text-slate-500 dark:text-slate-300">
+            <h3 className="mb-2">Net APY</h3>
+            <p className="font-bold text-black dark:text-white">
+              2.5<span className="text-slate-500 dark:text-slate-300">%</span>
+            </p>{' '}
+            {/* replace with actual value */}
+          </div>
+          <div className="mr-3 text-slate-500 dark:text-slate-300">
+            <h3 className="mb-2">Health Factor</h3>
+            <p className="font-bold text-orange-500">1.65</p> {/* replace with actual value */}
+          </div>
+        </div>
+      </div>
+    </motion.div>
   )
 }
--- integrations/aave/components/list-supplied-assets.tsx
+++ integrations/aave/components/list-supplied-assets.tsx
@@ -1,8 +1,76 @@
+import { useState } from 'react'
+
+import Image from 'next/image'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { MdHorizontalRule } from 'react-icons/md'
+
+import { Button } from '@/components/ui/button'
+import { Switch } from '@/components/ui/switch'
+
 export const ListSuppliedAssets = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true)
+
+  const handleCollapseClick = () => {
+    setIsCollapsed(!isCollapsed)
+  }
+
   return (
-    <div className="flex-1 bg-green-200 p-4 dark:bg-green-700">
-      <h2 className="mb-2 font-bold">Your Supplies</h2>
-      <div>Content goes here...</div>
+    <div className="flex-1 rounded border p-3 dark:border-slate-600">
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="font-bold">Your Supplies</h2>
+        <button className="flex items-center" onClick={handleCollapseClick}>
+          <span className="mr-2">{isCollapsed ? 'Hide' : 'Show'}</span>
+          {isCollapsed ? <MdHorizontalRule size={20} /> : <AiOutlinePlus size={20} />}
+        </button>
+      </div>
+      <div className="flex items-center">
+        <div className="mr-2 rounded border bg-white py-2 px-4 dark:border-slate-600 dark:bg-gray-800">
+          <h3 className="text-xs font-bold">
+            <span className="text-slate-500 dark:text-slate-300"> Balance $ </span>4.00
+          </h3>
+        </div>
+        <div className="mr-2 rounded border bg-white py-2 px-4 dark:border-slate-600 dark:bg-gray-800">
+          <h3 className="text-xs font-bold">
+            <span className="text-slate-500 dark:text-slate-300">APY</span> 2.5 <span className="text-slate-500 dark:text-slate-300">%</span>
+          </h3>
+        </div>
+        <div className="mr-2 rounded border bg-white py-2 px-4 dark:border-slate-600 dark:bg-gray-800">
+          <h3 className="text-xs font-bold">
+            <span className="text-slate-500 dark:text-slate-300"> Collateral $ </span>4.00
+          </h3>
+        </div>
+      </div>
+      {isCollapsed && (
+        <div className="overflow-x-auto">
+          <table className="mt-7 w-full table-auto border-collapse text-left">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 text-xs text-slate-500 dark:text-slate-300">Asset</th>
+                <th className="px-4 py-2 text-xs text-slate-500 dark:text-slate-300">Balance</th>
+                <th className="px-4 py-2 text-xs text-slate-500 dark:text-slate-300">APY</th>
+                <th className="px-4 py-2 text-xs text-slate-500 dark:text-slate-300">Collateral</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="mt-2 flex items-center px-4 py-2">
+                  <Image alt="Ethereum" className="mr-2 rounded-full" height={25} src="/integrations/connext/logos/chains/ethereum.png" width={25} />
+                  ETH
+                </td>
+                <td className="px-4 py-2">1.0121</td>
+                <td className="px-4 py-2">3%</td>
+                <td className="px-4 py-2">
+                  <Switch color="green" />
+                </td>
+                <td className="px-4 py-2">
+                  <Button className="mr-2">Withdraw</Button>
+                  <Button>Supply</Button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   )
 }
