aebd99cdfe11ca793e0068b9d782f6eb565f5959
Vitor Marthendal Nunes <marthendalnunes@gmail.com>
2023-07-18 10:21:07 -0300

    Update integrations/livepeer/components/button-share.tsx

--- integrations/livepeer/components/button-share.tsx
+++ integrations/livepeer/components/button-share.tsx
@@ -11,6 +11,7 @@ const assetOnDescription = {
 }
 
 export function ButtonShare({ href, PlayerType }: { href: string; PlayerType: PlayerType }) {
+  const url = href.startsWith('/') ? `${origin}/${href}` : href
   const description = `You can now paste the ${assetOnDescription[PlayerType]} URL`
 
   const { toast, dismiss } = useToast()
@@ -26,7 +27,7 @@ export function ButtonShare({ href, PlayerType }: { href: string; PlayerType: Pl
     }, 4200)
   }
   return (
-    <CopyToClipboard text={href}>
+    <CopyToClipboard text={url}>
       <span
         className="btn btn-emerald mt-4 flex w-full cursor-pointer rounded-md"
         onClick={() => {
