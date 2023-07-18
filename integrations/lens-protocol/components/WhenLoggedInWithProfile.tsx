4d489dc85faa40a5c57811ac08380a649cdac60a
Abhishek Naidu <geekyabhi12@gmail.com>
2023-07-19 00:36:24 +0530

    chore: add search and publications for the non lens users

--- integrations/lens-protocol/components/WhenLoggedInWithProfile.tsx
+++ integrations/lens-protocol/components/WhenLoggedInWithProfile.tsx
@@ -1,5 +1,7 @@
 import { ProfileOwnedByMe, useActiveProfile, useActiveWallet, WalletData } from '@lens-protocol/react-web'
 import { ReactNode } from 'react'
+import SearchProfile from './SearchProfile'
+import SearchPublication from './SearchPublication'
 
 type LoggedInConfig = {
   wallet: WalletData
@@ -24,25 +26,29 @@ export function WhenLoggedInWithProfile({ children }: WhenLoggedInWithProfilePro
 
   if (profile === null || error) {
     return (
-      <div className="flex flex-col items-center">
-        <img
-          alt="Logo"
-          className="w-16 h-16 rounded-full mb-2"
-          height={64}
-          src={`https://static-assets.lenster.xyz/images/brands/lens.png`}
-          width={64}
-        />
-        <div className="text-xl font-bold text-white">Claim your Lens profile 🌿</div>
-        <div className="space-y-1">
-          <div className="my-3">
-            Visit{' '}
-            <a className="font-bold" href="https://claim.lens.xyz" rel="noreferrer noopener" target="_blank">
-              claiming site
-            </a>{' '}
-            to claim your profile now 🏃‍♂️
+      <>
+        <div className="flex flex-col items-center">
+          <img
+            alt="Logo"
+            className="w-16 h-16 rounded-full mb-2"
+            height={64}
+            src={`https://static-assets.lenster.xyz/images/brands/lens.png`}
+            width={64}
+          />
+          <div className="text-xl font-bold text-white">Claim your Lens profile 🌿</div>
+          <div className="space-y-1">
+            <div className="my-3">
+              Visit{' '}
+              <a className="font-bold" href="https://claim.lens.xyz" rel="noreferrer noopener" target="_blank">
+                claiming site
+              </a>{' '}
+              to claim your profile now 🏃‍♂️
+            </div>
           </div>
         </div>
-      </div>
+        <SearchProfile />
+        <SearchPublication />
+      </>
     )
     // TODO guide user to create profile
     return null
