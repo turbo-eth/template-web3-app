024160a9540ff5124a67911b44fa4fd54ebe16c5
Thomas <tombutler4@hotmail.com>
2023-07-21 19:42:05 -0500

    feat: added example paywall page

---
+++ app/(general)/integration/unlock/paywall/page.tsx
@@ -0,0 +1,10 @@
+import PaywallScript from '@/integrations/unlock/components/paywall-script'
+
+export default function PaywallPage() {
+
+  return (
+    <div>
+      <PaywallScript />
+    </div>
+  )
+}
