e0e5b3f2ba98ad361f83209595814abd64002294
Vitor <marthendalnunes@gmail.com>
2023-08-22 08:27:36 -0300

    fix: add binance link to Ar exchange

--- integrations/arweave/components/insufficient-balance-error.tsx
+++ integrations/arweave/components/insufficient-balance-error.tsx
@@ -1,12 +1,11 @@
 import { LinkComponent } from '@/components/shared/link-component'
 
-// TODO: change the exchange name and url
 export const InsufficientBalanceError = () => {
   return (
     <div className="my-2 text-sm text-red-500">
       Insuffient balance. you can buy $AR at
-      <LinkComponent isExternal className="link px-1" href={'https://'}>
-        FOO exchange
+      <LinkComponent isExternal className="link px-1" href={'https://binance.com'}>
+        Binance
       </LinkComponent>
       and transfer to your Arweave wallet.
     </div>
