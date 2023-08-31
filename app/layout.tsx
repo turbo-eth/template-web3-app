02602d3fcade714f0d82f93d3639432c4f27982d
Vitor Marthendal Nunes <marthendalnunes@gmail.com>
2023-08-31 10:54:00 -0300

    Feat: Add pwa with next-pwa (#160)
    * feat: add pwa with next-pwa
    
    * fix: format
    
    * chore: update pnpm-lock.yaml
    
    * fix: formatting
    
    ---------
    
    Co-authored-by: Kames <sudo@kames.me>

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,11 @@ export const metadata = {
   metadataBase: new URL(url),
   title: `${siteConfig.name} - ${siteConfig.description}`,
   description: siteConfig.description,
+  manifest: "/manifest.json",
   icons: {
     icon: "/favicon.ico",
   },
+  themeColor: "#feefc4",
   openGraph: {
     title: siteConfig.name,
     description: siteConfig.description,
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import "./env.mjs"
 
+import withPWA from "next-pwa"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -38,6 +40,12 @@ const nextConfig = {
     })
     return config
   },
+  ...withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  }),
 }
 
 export default nextConfig
