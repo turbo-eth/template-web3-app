5997fd9a9311b063d095a49773b57fae1a864c14
TRBIV <trbiv@proton.me>
2023-09-05 17:10:18 -0500

    chore: generated wagmi

---
+++ integrations/unlock/wagmi.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@wagmi/cli'
+
+import { publicLockV13Abi } from './abis/public-lock-v13-abi'
+import { unlockV12Abi } from './abis/unlock-v12-abi'
+
+export default defineConfig({
+  out: 'integrations/unlock/generated/unlock-wagmi.ts',
+  contracts: [
+    {
+      name: 'UnlockV12',
+      abi: unlockV12Abi,
+    },
+    {
+      name: 'PublicLockV13',
+      abi: publicLockV13Abi,
+    },
+  ],
+  plugins: [],
+})
